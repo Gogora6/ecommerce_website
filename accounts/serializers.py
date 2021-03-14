3f26796ffc19d9c12e49b57ba42d3c6fe56aaec9
gogora6 <lasha.gogorishvili.1@btu.edu.ge>
2021-03-14 23:30:27 +0400

    changed database from sqlite to postgresql

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -30,6 +30,7 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         password = validated_data.pop('password')
+        validated_data.pop('password2')
         user = User(**validated_data)
         user.set_password(password)
         user.save()
